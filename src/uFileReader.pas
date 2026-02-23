e2dcdcd6df42b696d3b1f47218c4405f81c1725e
RokPerkovic <96005049+RokPerkovic@users.noreply.github.com>
2026-02-23 23:01:16 +0100

    Simplify file reading

--- src/uFileReader.pas
+++ src/uFileReader.pas
@@ -5,104 +5,35 @@ interface
 uses SysUtils, Classes;
 
 // class TFileReader
-const BuffSize = 512; 
 
 type 
 	TFileReader = class
-	
 	private
 		FFileName: String;
-		//FFile: File;
-		FBuff: array of byte;
-		FFileStream: TFileStream;
-		FBytesRead, FBuffPos: Integer;
-		FInput: AnsiString;
-	
-		procedure Init();
-		
 	public
 		constructor Create(FileName: String);
-		destructor Destroy(); override;
-
-		// reads file in blocks of size BuffSize, returns a next line on each NextLine function call
-		function NextLine(): AnsiString;
+		function LoadFile(): String;
 end;
-
 
 implementation
 
 constructor TFileReader.Create(FileName: String);
 begin
 	FFileName := FileName;
-	Init;
 end;
 
-destructor TFileReader.Destroy();
-begin
-	FFileStream.Free;
-	inherited Destroy;
-	//WriteLn('FileReader destroyed');
-end;
 
-procedure TFileReader.Init();
-begin
-	SetLength(FBuff, BuffSize);
-	FInput := '';
-	
-	try
-		FFileStream := TFileStream.Create(FFileName, fmOpenRead);
-	except
-		on E: EFOpenError do
-		begin
-			WriteLn('Error reading file ' + FFileName + ' because it is already used by another process.');
-			exit;
-		end;
-	end;
-	
-	FBuffPos := 0;
-	FBytesRead := 0;
-end;
-
-function TFileReader.NextLine(): AnsiString;
+function TFileReader.LoadFile(): String;
 var
-	Line, Block: AnsiString;
-	NewLinePos: Integer;
-	EOF: Boolean;
+  SL: TStringList;
 begin
-	EOF := False;
-	
-	while True do
-	begin
-		NewLinePos := Pos(#13#10, FInput);
-		
-		if NewLinePos > 0 then 
-		begin
-			Line := Copy(FInput, 1, NewLinePos + 1);
-			Delete(FInput, 1, NewLinePos + Length(sLineBreak) - 1);
-			Result := Line;
-			exit;
-		end;
-		
-		if EOF then
-		begin
-			Result := FInput;
-			FInput := '';
-			exit;
-		end;
-		
-		FBytesRead := FFileStream.Read(FBuff[0], BuffSize);
-		
-		if FBytesRead = 0 then
-		begin
-			EOF := True;
-			Continue;
-		end;
-		
-		SetString(Block, PAnsiChar(@FBuff[0]), FBytesRead);
-		FInput := FInput + Block;
-	end;
-
-	Result := Line;
+	SL := TStringList.Create;
+  try
+    SL.LoadFromFile(FFileName, TEncoding.UTF8);  // Load as UTF-8
+    Result := SL.Text;  // Join all lines into a single string
+  finally
+    SL.Free;
+  end;
 end;
 
 end.
